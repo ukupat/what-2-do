b3f9f604a1c125db9398714880ab98a2e55c6867
Uku <ukupat@gmail.com>
2014-05-17 13:30:30 +0300

    File browsing

--- src/gui/listener/ButtonListener.java
+++ src/gui/listener/ButtonListener.java
@@ -1,5 +1,9 @@
 package gui.listener;
 
+import gui.panel.InputPanel;
+import gui.util.FileBrowser;
+
+import javax.swing.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -7,6 +11,11 @@ public class ButtonListener implements MouseListener {
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
+		JButton button = (JButton) e.getSource();
+
+		if (button == InputPanel.chooseFileButton) {
+			new FileBrowser(); // TODO
+		}
 	}
 
 	@Override
---
+++ src/gui/util/FileBrowser.java
@@ -0,0 +1,36 @@
+package gui.util;
+
+import org.apache.log4j.Logger;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
+public class FileBrowser extends JFrame {
+
+	private final Logger log = Logger.getLogger(FileBrowser.class);
+
+	private File selectedDir;
+
+	public FileBrowser() {
+		createFileChooser();
+	}
+
+	private void createFileChooser() {
+		FileDialog fileDialog = new FileDialog(this);
+
+		fileDialog.setMode(JFileChooser.FILES_ONLY);
+		fileDialog.setVisible(true);
+
+		String fileName = fileDialog.getFile();
+
+		if (fileName != null) {
+			selectedDir = new File(fileDialog.getDirectory() + "/" + fileName);
+			log.info("User chose file " + selectedDir.getAbsolutePath());
+		}
+	}
+
+	public File getSelectedDirectory() {
+		return new File(selectedDir.getAbsolutePath());
+	}
+}

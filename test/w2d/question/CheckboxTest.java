bce50f1b3c3e4a95388e4f1d737900d8f423792a
Joanna Niklus <joanna.niklus@gmail.com>
2014-05-25 23:24:26 +0300

    Checkbox tests.

--- test/w2d/question/CheckboxTest.java
+++ test/w2d/question/CheckboxTest.java
@@ -1,5 +1,8 @@
-package w2d.question;
+package test.w2d.question;
 
+import org.junit.Before;
+import org.junit.Test;
+import w2d.antlr.ExceptionErrorStrategy;
 import org.junit.Test;
 import w2d.parser.W2D;
 import w2d.parser.question.QuestionParser;
@@ -9,7 +12,13 @@ import static org.junit.Assert.assertNotNull;
 
 public class CheckboxTest {
 
-	@Test
+    @Before
+    public void cleanErrors() {
+        ExceptionErrorStrategy.cleanErrors();
+        W2D.resetParsers();
+    }
+
+    @Test
 	public void oneLineCheckbox() {
 		new W2D("#c = new Checkbox {label:\"You cool?\"};").readTheScript();
 
@@ -17,4 +26,30 @@ public class CheckboxTest {
 		assertNotNull(QuestionParser.questions.get("#c"));
 		assertEquals("You cool?", QuestionParser.questions.get("#c").label);
 	}
+
+    @Test
+    public void multiLineCheckbox() {
+        new W2D("#c = new Checkbox {\n\tlabel: \"You cool?\"\n};").readTheScript();
+
+        assertEquals(1, QuestionParser.questions.size());
+        assertNotNull(QuestionParser.questions.get("#c"));
+        assertEquals("You cool?", QuestionParser.questions.get("#c").label);
+    }
+
+    @Test
+    public void noWhitespaceCheckbox() {
+        new W2D("#c=new CheckBox{label:\"You cool?\"};").readTheScript();
+
+        assertEquals(1, QuestionParser.questions.size());
+        assertNotNull(QuestionParser.questions.get("#c"));
+        assertEquals("You cool?", QuestionParser.questions.get("#c").label);
+    }
+
+    @Test
+    public void noSemicolonCheckbox() {
+        new W2D("#c = new Checkbox {label: \"Hi!\"}").readTheScript();
+
+        assertFalse(ExceptionErrorStrategy.errors.isEmpty());
+    }
+
 }
